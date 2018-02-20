c30f2e09615be95c4b230bd00fa4560dac9d9d84
Justice Otuya <jotuya2@gmail.com>
2018-02-20 20:20:08 +0100

    Day one work

--- designs.js
+++ designs.js
@@ -3,8 +3,46 @@
 
 // When size is submitted by the user, call makeGrid()
 
+//Select size input
+var gridHeight = document.getElementById("inputHeight");
+gridHeight.addEventListener("change paste keyup", function(e) {});
+
+var gridWidth = document.getElementById("inputWeight");
+gridWidth.addEventListener("change paste keyup", function(e) {});
+
+var submit = document.querySelector("input#submit");
+submit.addEventListener("click", function() {
+  gridHeightValue = gridHeight.value;
+  gridWidthValue = gridWidth.value;
+  colorValue = color.value;
+  console.log("Grid Height : " + gridHeightValue);
+  console.log("Grid Weight : " + gridWidthValue);
+  console.log(colorValue);
+  makeGrid();
+});
+
+//select color input
+var color = document.getElementById("colorPicker");
+color.addEventListener("change", function() {});
+
+//make grid
 function makeGrid() {
+  var table = document.querySelector("table");
 
-// Your code goes here!
+  for (var i = 0; i < gridHeight.value; i++) {
+    var tr = document.createElement("tr");
+    tr = table.insertRow(-1);
 
+    for (var j = 0; j < gridWidth.value; j++) {
+      var td = document.createElement("td");
+      td = tr.insertCell(-1);
+    }
+  }
+}
+
+function clearGrid() {
+  var reset = document.querySelector("#reset");
+  reset.addEventListener("click", function() {
+    $("td").remove();
+  });
 }
